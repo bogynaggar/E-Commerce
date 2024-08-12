1f32c4a45205d9773991bfc29c4325a8cd302b48
bogynaggar <bogynaggar@gmail.com>
2024-08-12 14:15:20 +0300

    design profile fragment /dark mode

--- app/src/main/java/com/example/e_commerceapp/ProfileFragment.kt
+++ app/src/main/java/com/example/e_commerceapp/ProfileFragment.kt
@@ -20,12 +20,23 @@ class ProfileFragment :Fragment(){
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(
-            inflater , container , false
-        )
+            inflater , container , false)
 
         binding.profileSection.setOnClickListener {
             val intent = Intent(context , User_profile::class.java)
             startActivity(intent)
+        }
+
+
+        binding.shareSection.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
 
 
